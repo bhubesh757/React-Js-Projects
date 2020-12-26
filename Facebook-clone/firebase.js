8cffda57d88edb8c02830907c4de22d09875fd95
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2020-12-26 10:21:53 +0000

    Add files via upload

--- Facebook-clone/firebase.js
+++ Facebook-clone/firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyBttCR36-CiAlR5habQGEZ2HD1wm1xJY5Q",
     authDomain: "facebook-878ed.firebaseapp.com",
@@ -8,3 +11,13 @@ const firebaseConfig = {
     appId: "1:162788120934:web:675947da80992205ec10a7",
     measurementId: "G-HFTJHTT8MP"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth , provider }
+
+export default db;

b45df4b8a685ef325098b573bcac7f8a3995b352
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2020-12-30 03:11:06 +0000

    Add files via upload

---
+++ LinkedIn-Clone/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/userSlice';
+
+export default configureStore({
+  reducer: {
+    user: counterReducer,
+  },
+});
